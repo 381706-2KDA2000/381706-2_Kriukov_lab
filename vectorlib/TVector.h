40eb2c2c7f0b4e5af8b00e1712547d77b0c12639
381706-2KDA2000 <43183898+381706-2KDA2000@users.noreply.github.com>
2018-12-21 10:38:05 +0300

    последняя стрелочка и починенный вектор

--- vectorlib/TVector.h
+++ vectorlib/TVector.h
@@ -119,7 +119,7 @@ int TVector<ValType>::operator==(const TVector & v)
 template<class ValType>
 TVector<ValType> & TVector<ValType>::operator=(const TVector &v)
 {
-  if (v.pVector != >pVector)
+  if (v.pVector != pVector)
   {
     size = v.size;
     startIndex = v.startIndex;
